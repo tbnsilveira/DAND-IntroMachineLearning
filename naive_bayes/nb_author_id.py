41ace88100a45853b9c75aec8ad96679bb1da8b5
tbnsilveira <tiagodasilveira@gmail.com>
2018-05-29 00:22:37 -0300

    feat: code for naive bayes mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,8 +13,8 @@
 import sys
 from time import time
 sys.path.append("../tools/")
+## sys.path.append("/home/tbnsilveira/workplace/tef_udacity/7. Intro to Machine Learning/miniProjects/ud120-projects/tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -22,12 +22,22 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+## Treinando o modelo:
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+## Testando o modelo:
+t1 = time()
+clf.predict(features_test)
+print("training time:", round(time()-t1, 3), "s")
+
+## Avaliando o resultado:
+clf.score(features_test,labels_test)
 
 #########################################################
-
-
