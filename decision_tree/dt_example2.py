0746989576adbc6ea6c4d7893373098e916278b5
tbnsilveira <tiagodasilveira@gmail.com>
2018-05-29 21:54:01 -0300

    feat: short decision tree algorithms implemented in class

---
+++ decision_tree/dt_example2.py
@@ -0,0 +1,31 @@
+import sys
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+
+import numpy as np
+import pylab as pl
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+
+#################################################################################
+
+
+########################## DECISION TREE #################################
+
+
+
+#### your code goes here
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+acc = clf.score(features_test, labels_test)
+### be sure to compute the accuracy on the test set
+    
+def submitAccuracies():
+  return {"acc":round(acc,3)}
+
