e5987993bc51100b938a15558e1ddf307d72476e
tbnsilveira <tiagodasilveira@gmail.com>
2018-05-30 01:32:16 -0300

    refactor: changing back the SelectorPercentile to 10 percent

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -13,17 +13,43 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+#########################################################
+### your code goes here ###
+from sklearn import tree
+
+### Defining the classifier
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+### Training the decision tree
+clf = clf.fit(features_train, labels_train)
+
+### Predicting
+pred = clf.predict(features_test)
+
+### Evaluating accuracy
+acc_min_samples_split_40 = clf.score(features_test, labels_test)
+print(acc_min_samples_split_40)
 
 
 #########################################################
-### your code goes here ###
+### How many features are in the data?
+len(features_train)
+len(features_train[1])
+features_train.shape
+# Data has 3785 attributes!
+
+
+
+
+
+
+
 
 
 #########################################################
