dd6dcc1bd666b91e1a626915873a871718de3dd7
tbnsilveira <tiagodasilveira@gmail.com>
2018-06-06 17:51:28 -0300

    feat: finished regression code from Lesson7

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -48,28 +48,18 @@ print('Regression score on the training data: ',reg.score(feature_train,target_t
 print('Regression score on the testing data: ',reg.score(feature_test,target_test))
 
 #%% Trying another hypothesis: performing the regression of bonus against long term incentive
-trial2_features_list = ["bonus", "salary"]
+features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
-#%% training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
 test_color = "r"
 
-#%% Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
-from sklearn import linear_model
 reg = linear_model.LinearRegression()
 reg.fit (feature_train, target_train)
 
-
-
-
-
+print('Regression score on the testing data, considering LTI: ',reg.score(feature_test,target_test))
 
 #%% draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -91,6 +81,11 @@ try:
 except NameError:
     pass
 plt.xlabel(features_list[1])
+
+## These two lines above are just to show the difference when training without outliers:
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
