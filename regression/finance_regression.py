e1f143a7e64df4ef5acf968769c46b18c36e4ab6
tbnsilveira <tiagodasilveira@gmail.com>
2018-06-06 17:33:26 -0300

    feat: initial code for regression using sklearn

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -12,40 +12,66 @@
     You fill in the regression code where indicated:
 """    
 
-
+#%% Using Conda-Spyder cells on python code:
 import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
+#%% Feature extraction
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
-### training-testing split needed in regression, just like classification
+#%% training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
-
-
-### Your regression goes here!
+#%% Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit (feature_train, target_train)
+
+print('Regression slope: ',reg.coef_)
+print('Regression intercept: ',reg.intercept_)
+
+#%% Evaluating the linear regression:
+print('Regression score on the training data: ',reg.score(feature_train,target_train))
+print('Regression score on the testing data: ',reg.score(feature_test,target_test))
+
+#%% Trying another hypothesis: performing the regression of bonus against long term incentive
+trial2_features_list = ["bonus", "salary"]
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+target, features = targetFeatureSplit( data )
+
+#%% training-testing split needed in regression, just like classification
+from sklearn.cross_validation import train_test_split
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+train_color = "b"
+test_color = "r"
+
+#%% Your regression goes here!
+### Please name it reg, so that the plotting code below picks it up and 
+### plots it correctly. Don't forget to change the test_color above from "b" to
+### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit (feature_train, target_train)
 
 
 
 
 
 
-
-
-### draw the scatterplot, with color-coded training and testing points
+#%% draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
     plt.scatter( feature, target, color=test_color ) 
