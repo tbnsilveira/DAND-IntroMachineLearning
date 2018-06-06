25aaaf259ab188430aad05a795a8c6d49555247a
tbnsilveira <tiagodasilveira@gmail.com>
2018-06-06 19:26:53 -0300

    feat: initial code for outlier removal

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -5,14 +5,44 @@ import numpy
 import matplotlib.pyplot as plt
 import pickle
 
-from outlier_cleaner import outlierCleaner
+from sklearn import linear_model
 
 
-### load up some practice data with outliers in it
-ages = pickle.load( open("practice_outliers_ages.pkl", "r") )
-net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
+#%% There was an error importing the file. Declaring the function here:
+#from outlier_cleaner.py import outlierCleaner
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
 
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    
+    cleaned_data = []
 
+    ## Calculating the error:
+    errors = net_worths - predictions
+    ## Creating a dictionary for error position on the lists:
+    errorPos = {}
+    for ix, error in enumerate(errors):
+        errorPos[str(error)] = ix
+    
+    errorsSort = errors[:] 
+    errorsSort.sort(axis=0)  #Sorting the errors values
+    errorsSort = errors[9:]  #Selecting the 81 higher elements
+    
+    for error in errorsSort:
+        ix = errorPos[str(error)]
+        sample = (ages[ix], net_worths[ix], errors[ix])
+        cleaned_data.append(sample)
+        
+    return cleaned_data
+
+#%% load up some practice data with outliers in it
+ages = pickle.load( open("../outliers/practice_outliers_ages.pkl", "r") )
+net_worths = pickle.load( open("../outliers/practice_outliers_net_worths.pkl", "r") )
 
 ### ages and net_worths need to be reshaped into 2D numpy arrays
 ### second argument of reshape command is a tuple of integers: (n_rows, n_columns)
@@ -20,22 +50,23 @@ net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 ### and n_columns is the number of features
 ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
 net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
+
 from sklearn.cross_validation import train_test_split
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
-### fill in a regression here!  Name the regression object reg so that
+## Visualizing the points:
+plt.scatter(ages, net_worths)
+plt.show()
+
+#%% fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+reg = linear_model.LinearRegression()
+reg.fit (ages_train, net_worths_train)
 
+print('Regression score on the testing data: ',reg.score(ages_test,net_worths_test))
+print('The slope of the regression is ',reg.coef_)
 
-
-
-
-
-
-
-
-
-
+#%% Visualizing the trained data:
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
 except NameError:
@@ -44,7 +75,7 @@ plt.scatter(ages, net_worths)
 plt.show()
 
 
-### identify and remove the most outlier-y points
+#%%identify and remove the most outlier-y points
 cleaned_data = []
 try:
     predictions = reg.predict(ages_train)
@@ -54,12 +85,7 @@ except NameError:
     print "can't make predictions to use in identifying outliers"
 
 
-
-
-
-
-
-### only run this code if cleaned_data is returning data
+#%% only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
     ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
