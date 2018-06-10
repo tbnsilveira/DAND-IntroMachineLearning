9dca91ca8bd23ec0543e116027c8f32fb4be7003
tbnsilveira <tiagodasilveira@gmail.com>
2018-06-10 17:09:27 -0300

    feat: cleaning away signature words at TextLearning code

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -23,19 +23,18 @@ from parse_out_email_text import parseOutText
 """
 
 
-from_sara  = open("from_sara.txt", "r")
-from_chris = open("from_chris.txt", "r")
+from_sara  = open("../text_learning/from_sara.txt", "r")
+from_chris = open("../text_learning/from_chris.txt", "r")
 
 from_data = []
 word_data = []
 
-### temp_counter is a way to speed up the development--there are
+#%%## temp_counter is a way to speed up the development--there are
 ### thousands of emails from Sara and Chris, so running over all of them
 ### can take a long time
 ### temp_counter helps you only look at the first 200 emails in the list so you
 ### can iterate your modifications quicker
 temp_counter = 0
-
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
@@ -48,14 +47,20 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            toremove = ["sara", "shackleton", "chris", "germani"]
+            for stopword in toremove:
+                text.replace(stopword,'')
 
             ### append the text to word_data
-
+            word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if (name == 'sara'):
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
@@ -63,8 +68,8 @@ print "emails processed"
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("../text_learning/your_word_data.pkl", "w") )
+pickle.dump( from_data, open("../text_learning/your_email_authors.pkl", "w") )
 
 
 
