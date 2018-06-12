123114bba086aecc2ff07ac1956e519dbffff9b4
tbnsilveira <tiagodasilveira@gmail.com>
2018-06-12 10:21:44 -0300

    feat: coding with cross_validation.train_test_split

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -29,15 +29,18 @@ labels, features = targetFeatureSplit(data)
 
 #%%## it's all yours from here forward!  
 from sklearn import tree
+from sklearn.cross_validation import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
 ### Defining the classifier
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features, labels)
+clf = clf.fit(features_train, labels_train)
 
 ### Predicting
-pred = clf.predict(features)
+pred = clf.predict(features_test)
 
 ### Evaluating accuracy
-acc = clf.score(features, labels)
+acc = clf.score(features_test, labels_test)
 print(acc)
 
