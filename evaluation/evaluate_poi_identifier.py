7e992ad069cb174a79022c580efe953b5c1fb117
tbnsilveira <tiagodasilveira@gmail.com>
2018-06-12 18:14:16 -0300

    feat: code for evaluation metrics

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -25,7 +25,37 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+#%%## it's all yours from here forward!  
+from sklearn import tree
+from sklearn.cross_validation import train_test_split
 
-### your code goes here 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### Defining the classifier
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+### Predicting
+pred = clf.predict(features_test)
+
+### Evaluating accuracy
+acc = clf.score(features_test, labels_test)
+print(acc)
+
+#%% Evaluating (Class 31. Number of TP)
+## Look at the predictions of your model and compare them to the true test labels. Do you get any true positives? 
+## (In this case, we define a true positive as a case where both the actual label and the predicted label are 1)
+print('Pred \t Real')
+for i in range(len(pred)):
+    print('{}\t{}'.format(pred[i],labels_test[i]))
+
+
+
+
+
+
+
+
+
 
 
